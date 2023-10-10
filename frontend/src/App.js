64c3502c982e438971cf08259c6f719bbe003fbc
Shreyashs98 <104050227+Shreyashs98@users.noreply.github.com>
2023-10-10 19:50:24 +0530

    Added Login Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,35 @@
-import React from 'react';
 import './App.css';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import SignupPage from './pages/SignupPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage></SignupPage>,
+  },
+]);
+
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <RouterProvider router={router} />
     </div>
   );
 }
