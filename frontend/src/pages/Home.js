1705fa9930359acdfdbee45c884902502f43e87f
Shreyashs98 <104050227+Shreyashs98@users.noreply.github.com>
2023-10-10 17:29:04 +0530

    Home page added

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+import ProductList from "../features/product-list/ProductList";
+import NavBar from "../features/navbar/Navbar";
+const Home = () => {
+    return ( 
+        <div>
+            <NavBar>
+                <ProductList></ProductList>
+            </NavBar>
+        </div>
+     );
+}
+ 
+export default Home;
