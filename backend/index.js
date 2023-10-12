fbaffb77f8b559b53c5f37651fcae697bde7276c
Shreyashs98 <104050227+Shreyashs98@users.noreply.github.com>
2023-10-12 21:18:29 +0530

    mvc of products

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,26 @@
-console.log("Hello")
+const express = require('express');
+const server = express();
+const mongoose = require('mongoose');
+const { createProduct } = require('./controller/Product');
+const productsRouters = require('./routes/Products');
+//middlewares
+
+server.use(express.json());//to parse req.body
+server.use('/products',productsRouters.router)
+main().catch(err=> console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/ecommerce');
+    console.log('database connected')
+}
+
+server.get('/',(req,res)=>{
+    res.json({status:'success'})
+
+})
+
+server.post('/products',createProduct);
+
+server.listen(8080,()=>{
+    console.log('server started')
+})
