d5c9bf9933ac7b4d5f796b44faa859e37b670131
Shreyashs98 <104050227+Shreyashs98@users.noreply.github.com>
2023-10-14 11:22:13 +0530

    removed unwanted

--- frontend/src/pages/Chatbot/Chatbot.js
+++ frontend/src/pages/Chatbot/Chatbot.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import "./Chatbot.css"; // Import the external CSS file
 import NavBar from "../../features/navbar/Navbar";
 
