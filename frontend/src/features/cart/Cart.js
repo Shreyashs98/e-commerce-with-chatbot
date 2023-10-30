ee1ad57bfab06e565d98a6913fb73a02f70cb1c4
Shreyashs98 <104050227+Shreyashs98@users.noreply.github.com>
2023-10-30 13:50:38 +0530

    empty cart

--- frontend/src/features/cart/Cart.js
+++ frontend/src/features/cart/Cart.js
@@ -7,7 +7,7 @@ import {
   updateCartAsync,
 } from "./cartSlice";
 import { Link } from "react-router-dom";
-import { Navigate } from "react-router-dom";
+// import { Navigate } from "react-router-dom";
 import { discountedPrice } from "../../app/constants";
 import { Grid } from "react-loader-spinner";
 import Modal from "../common/Modal";
