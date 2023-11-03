9572e30116d73627a3a15ff29f947aa97d09bd03
Shreyashs98 <104050227+Shreyashs98@users.noreply.github.com>
2023-11-03 11:07:18 +0530

    signout bug fixed

--- frontend/src/features/auth/components/Logout.js
+++ frontend/src/features/auth/components/Logout.js
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { selectLoggedInUser, signOutAsync } from "../authSlice";
 import { useDispatch, useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
 
 function Logout() {
   const dispatch = useDispatch();
@@ -9,10 +8,13 @@ function Logout() {
 
   useEffect(() => {
     dispatch(signOutAsync());
-  });
+    
+    if (!user) {
+      window.location.href = "/login";
+    }
+  }, [dispatch, user]);
 
-  // but useEffect runs after render, so we have to delay navigate part
-  return <>{!user && <Navigate to="/login" replace={true}></Navigate>}</>;
+  return null; 
 }
 
 export default Logout;
